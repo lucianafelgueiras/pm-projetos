ec6697f43fc4a371c938b9001b7ebf1a1a8bfcf6
Luciana Felgueiras <fel.luciana@yahoo.com.br>
2017-09-05 16:52:00 -0300

    inicio proj 012

---
+++ pm-proj012/js/comportamento.js
@@ -0,0 +1,12 @@
+let botao = document.querySelectorAll("button");
+
+let tela = document.querySelector("painel");
+
+let clear = document.querySelector("clear");
+
+
+('button')document.getElementById('figBot').addEventListener('click', altBut());
+
+function altBut(){
+    
+}
