165f419b3fc7d94af85e267fe04c5335b9069cf5
Luciana Felgueiras <fel.luciana@yahoo.com.br>
2017-09-08 13:45:24 -0300

    versao 2 proj 012

--- pm-proj012/js/comportamento.js
+++ pm-proj012/js/comportamento.js
@@ -1,12 +1,62 @@
-let botao = document.querySelectorAll("button");
+function $("elemento"){ // pega o elemento//
+    return document.querySelector("elemento");
+}
 
-let tela = document.querySelector("painel");
-
-let clear = document.querySelector("clear");
-
-
-('button')document.getElementById('figBot').addEventListener('click', altBut());
-
-function altBut(){
+function trocar(){  //Azul, Laranja, Verde //
+    $("#figBot").classList.add("converter");
     
+    if("#figBot" == ".converter"){
+       document.getElementById("#figBot").classList.remove("converter"); 
+       document.getElementById("#figBot").classList.add("convertido"); 
+    }else if("#figBot" == ".convertido"){
+        $("#figBot").classList.remove("convertido");
+    }
+}
+
+function tecla(){
+    var valor = $(".numBot").value;
+    return valor; 
+}
+
+// evento botões //
+document.getElementById("#figBot").addEventListener("Click", function()){
+    trocar();
+}
+$(".numBot").addEventListener("Click", function(){
+    tecla();
+}
+
+// teclado //
+if(this.valor == ""){
+    alert("Erro, digite um valor válido!!")
+}
+else if(this.valor == "0"){
+    ("#display").value = tecla();
+}
+else if(this.valor == "1"){
+    ("#display").value = tecla();
+}
+else if(this.valor == "2"){
+    ("#display").value = tecla();
+}
+else if(this.valor == "3"){
+    ("#display").value = tecla();
+}
+else if(this.valor == "4"){
+    ("#display").value = tecla();
+}
+else if(this.valor == "5"){
+    ("#display").value = tecla();
+}
+else if(this.valor == "6"){
+    ("#display").value = tecla();
+}
+else if(this.valor == "7"){
+    ("#display").value = tecla();
+}
+else if(this.valor == "8"){
+    ("#display").value = tecla();
+}
+else (this.valor == "9"){
+    ("#display").value = tecla();
 }
